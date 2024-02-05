6e693c9d1aa9a379ec7470fdee22a6189cc70b64
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2024-02-05 18:23:16 +0000

    ne

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -14,7 +14,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 
 		a = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		b = tree->right ? 1 + binary_tree_height(tree->right) : 0;
-		return ((a > r) ? a : b);
+		return ((a > b) ? a : b);
 	}
 	return (0);
 }
