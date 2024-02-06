f108d8531c31150160f014643de603116e1d6288
Krieglerlawoko <krieglerlawokojudge@gmail.com>
2024-02-06 14:46:06 +0000

    ne

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,34 +1,46 @@
 #include "binary_trees.h"
-
+#include "102-binary_tree_is_complete.c"
 /**
- * binary_tree_rotate_left - Binary tree rotated left.
- * @tree: Points to root node of tree to rotate.
- *
- * Return: A pointer to the new root node after rotation.
- */
-binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
+ * check_max - goes through a binary tree cheking ropt as max value
+ * @tree: pointer to the root
+ * Return: 1 if all nodes are max, 0 in other cases
+ **/
+int check_max(const binary_tree_t *tree)
 {
-	binary_tree_t *pivot, *tmp, pivot;
+	int tm1 = 1, tm2 = 1;
 
-	if (tree == NULL || tree->right == NULL)
-		return (NULL);
-
-	pivot = tree->right;
-	tmp = pivot->left;
-	pivot->left = tree;
-	tree->right = tmp;
-	if (tmp != NULL)
-		tmp->parent = tree;
-	tmp = tree->parent;
-	tree->parent = pivot;
-	pivot->parent = tmp;
-	if (tmp != NULL)
+	if (!tree)
+		return (0);
+	if (!tree->right && !tree->left)
+		return (1);
+	if (tree->left)
 	{
-		if (tmp->left == tree)
-			tmp->left = pivot;
-		else
-			tmp->right = pivot;
+		if (tree->n <= tree->left->n)
+			return (0);
+		tmp = check_max(tree->left);
 	}
+	if (tree->right)
+	{
+		if (tree->n <= tree->right->n)
+			return (0);
+		tmp = check_max(tree->right);
+	}
+	return (tm1 && tm2);
+}
+/**
+ * binary_tree_is_heap - checks if a binary tree is heap
+ * @tree: pointer to the node
+ * Return: 1 in case BTS /  0 otherwise
+ **/
+int binary_tree_is_heap(const binary_tree_t *tree)
+{
+	int tm;
 
-	return (pivot);
+	if (!tree)
+		return (0);
+
+	tm = binary_tree_is_complete(tree);
+	if (!tm)
+		return (0);
+	return (check_max(tree));
 }
